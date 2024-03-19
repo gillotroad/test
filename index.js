19c25143eef0e091d91627e1ad19f80083596884
gillotroad <164035322+gillotroad@users.noreply.github.com>
2024-03-19 16:33:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 function initPano() {
-  // Note: constructed panorama objects have visible: true
-  // set by default.
-  const panorama = new google.maps.StreetViewPanorama(
-    document.getElementById("map"),
-    {
-      position: { lat: 38.7211228, lng: -106.7826051 },
-      addressControl: false,
-      linksControl: true,
-      panControl: true,
-      enableCloseButton: false,
-    },
+
+    const product = urlParams.get('product')
   );
 }
 
