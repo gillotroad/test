f81fed53fdcfc2fbab4b7f03fae268db7106f631
gillotroad <164035322+gillotroad@users.noreply.github.com>
2024-03-19 16:35:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-function initPano() {
+const firstparam = urlParams.get('long')
 
-    const product = urlParams.get('product')
-  );
-}
-
-window.initPano = initPano;
+document.write(firstparam)
